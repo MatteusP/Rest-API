873ab7320ba1b109f2a7fc4f0a14ef711468143a
Mateus Paiva <matteusp@alu.ufc.br>
2022-10-23 11:30:07 -0300

    adicionado dependências e configurando banco

---
+++ spring-boot-postgres/src/test/java/APIRest/springbootpostgres/SpringBootPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package APIRest.springbootpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
